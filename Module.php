dd8064e2a4471c0d23b9bfa277d72c88afae2d3a
Emmanuel Bouton <goten4@gmail.com>
2014-07-17 18:35:53 +0200

    FIX namespace Module.php

--- Module.php
+++ Module.php
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU General Public License
  * along with kamba.  If not, see <http://www.gnu.org/licenses/>.
  */
-namespace Permission;
+namespace KmbPermission;
 
 use Zend\EventManager\EventInterface;
 use Zend\ModuleManager\Feature\BootstrapListenerInterface;
